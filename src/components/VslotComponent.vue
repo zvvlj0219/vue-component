1d81221d7d8f190e974d1066120a1cc4d8d779db
zvvlj0219 <s00302448@gmail.com>
2021-09-17 16:13:09 +0900

    v-slot template

---
+++ src/components/VslotComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <slot name="hello"></slot>
+  </div>
+</template>
+
+<!-- v-slot（名前付きスロット）の使い方-->
+
+<!-- 
+①以下を親コンポーネントに追記
+- コンポーネント名は任意に指定できる VslotComponentのところ
+- v-slotのプロパティに任意のスロット名をわたす helloのところ
+- v-slot を使うときは絶対templateタグを使う 
+- v-slot を書くところはtemplateタグであってコンポーネントタグではない
+
+<VslotComponent >
+  <template v-slot:hello>
+    <p>HelloWorld</p>
+  </template>
+</VslotComponent>
+
+-->
+<!--
+②VslotComponent内
+コンポーネントのテンプレートの中に<slot></slot>を追記
+name="hello" をslotタグに追加 
+
+slotのところが親コンポーネント内の子コンポーネントタグ（VslotComponent）内に書いたhtmlに置換される
+
+<template>
+  <div>
+   <slot name="hello"></slot>
+  </div>
+</template>
+
+-->
